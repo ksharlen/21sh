f1519461065d3fffbcc101754b32da733a8d7f56
Kharazim Sharlene <ksharlen@oa-b1.21-school.ru>
2020-02-03 00:12:32 +0300

    added set_cursor_pos

--- input/include/input_termcap.h
+++ input/include/input_termcap.h
@@ -6,7 +6,7 @@
 /*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/02 21:07:07 by ksharlen          #+#    #+#             */
-/*   Updated: 2020/02/02 23:36:40 by ksharlen         ###   ########.fr       */
+/*   Updated: 2020/02/03 00:06:53 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,22 @@
 
 # define CO	"co"
 # define LI	"li"
+# define CM	"cm"
 
+/*
+**INTERFACE
+*/
 struct s_cursor		get_pos_cursor(void);
+struct s_win		get_win_size(void);
+void				set_cursor_pos(int col, int row);
+
+/*
+**INTERNAL
+*/
 void				input_tgetent(void);
 char				*input_tgetstr(char *cb);
 char				*input_tgoto(char *cb, int one, int two);
 void				input_tputs(const char *str, int affcnt, int (*putc)(int));
 int					input_tgetnum(char *str);
-struct s_win		get_win_size(void);
 
 #endif
--- input/src/termcap/input_common.c
+++ input/src/termcap/input_common.c
@@ -6,7 +6,7 @@
 /*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/02 21:06:48 by ksharlen          #+#    #+#             */
-/*   Updated: 2020/02/02 23:35:32 by ksharlen         ###   ########.fr       */
+/*   Updated: 2020/02/03 00:12:14 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,13 @@ struct s_win	get_win_size(void)
 	wn.cols = input_tgetnum(CO);
 	wn.rows = input_tgetnum(LI);
 	return (wn);
+}
+
+void	set_cursor_pos(int col, int row)
+{
+	char	*str;
+
+	str = input_tgetstr(CM);
+	str = input_tgoto(str, col, row);
+	input_tputs(str, 0, ft_putchar);
 }
