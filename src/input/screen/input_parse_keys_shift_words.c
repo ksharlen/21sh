75d0d3fae0649ddfeb5674e66841632379ba6e1e
ksharlen <miily@list.ru>
2020-02-05 23:52:31 +0300

    refact skip word

--- src/input/screen/input_parse_keys_shift_words.c
+++ src/input/screen/input_parse_keys_shift_words.c
@@ -6,13 +6,16 @@
 /*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/05 21:16:39 by ksharlen          #+#    #+#             */
-/*   Updated: 2020/02/05 23:38:05 by ksharlen         ###   ########.fr       */
+/*   Updated: 2020/02/05 23:50:31 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "input_parse_key.h"
 
-static size_t	get_skip_right_word(const char *str, size_t pos)
+#define SKIP_L_WORD 0
+#define SKIP_R_WORD 1
+
+static size_t	get_shift_skip_word(const char *str, size_t pos, int whome)
 {
 	size_t		shift;
 	const char	*p_str;
@@ -21,12 +24,12 @@ static size_t	get_skip_right_word(const char *str, size_t pos)
 	if (str)
 	{
 		p_str = &str[pos];
-		while (*p_str)
+		while (whome == SKIP_L_WORD ? p_str != str : *p_str)
 		{
 			if (((*p_str) > 32 && (*p_str) < 126) && *(p_str - 1) == ' ')
 				break ;
+			whome == SKIP_L_WORD ? --p_str : ++p_str;
 			++shift;
-			++p_str;
 		}
 		++shift;
 	}
@@ -39,13 +42,11 @@ static void parse_shft_right_arrow(struct s_input *inp)
 	size_t			shift;
 	size_t			pos;
 	char			*str;
-	// struct s_cursor	end;
-P_UNUSED(next_word);
-	// end = input_get_end_string(inp);
+
 	if (inp->gap.slide != inp->gap.len_string)
 	{
 		str = gap_get_buf(&inp->gap);
-		shift = get_skip_right_word(str, inp->gap.slide + 1);
+		shift = get_shift_skip_word(str, inp->gap.slide + 1, SKIP_R_WORD);
 		pos = inp->gap.slide + shift + inp->len_greet;
 		next_word.y = pos / inp->win.cols;
 		next_word.x = pos % inp->win.cols;
@@ -56,29 +57,6 @@ P_UNUSED(next_word);
 	}
 }
 
-//!i want to refact this func but i don't know how.
-static size_t	get_skip_left_word(const char *str, size_t pos)
-{
-	size_t		shift;
-	const char	*p_str;
-
-	shift = 0;
-	if (str)
-	{
-		p_str = &str[pos];
-		while (p_str != str)
-		{
-			if (((*p_str) > 32 && (*p_str) < 126) && *(p_str - 1) == ' ')
-				break ;
-			--p_str;
-			++shift;
-		}
-		++shift;
-	}
-	return (shift);
-}
-
-//!TMP i think here is bug
 static void	parse_shft_left_arrow(struct s_input *inp)
 {
 	struct s_cursor	prev_word;
@@ -90,7 +68,7 @@ static void	parse_shft_left_arrow(struct s_input *inp)
 	if (inp->gap.slide)
 	{
 		str = gap_get_buf(&inp->gap);
-		shift = get_skip_left_word(str, inp->gap.slide - 1);
+		shift = get_shift_skip_word(str, inp->gap.slide - 1, SKIP_L_WORD);
 		pos = inp->gap.slide - shift + inp->len_greet;
 		prev_word.y = pos / (inp->win.cols);
 		prev_word.x = pos % (inp->win.cols);
