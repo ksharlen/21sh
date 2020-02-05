fb2813c9ce2da520cb60b2a83ba9d703ae0bc31d
ksharlen <miily@list.ru>
2020-02-05 23:29:37 +0300

    done skip words, need refact

--- src/input/screen/input_parse_keys_shift_words.c
+++ src/input/screen/input_parse_keys_shift_words.c
@@ -6,15 +6,54 @@
 /*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/05 21:16:39 by ksharlen          #+#    #+#             */
-/*   Updated: 2020/02/05 23:09:41 by ksharlen         ###   ########.fr       */
+/*   Updated: 2020/02/05 23:28:12 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "input_parse_key.h"
 
+static size_t	get_skip_right_word(const char *str, size_t pos)
+{
+	size_t		shift;
+	const char	*p_str;
+
+	shift = 0;
+	if (str)
+	{
+		p_str = &str[pos + 1];
+		while (*p_str)
+		{
+			if (*(p_str - 1) == ' ' && (*(p_str) > 32 && *(p_str) <= 126))
+				break ;
+			++shift;
+			++p_str;
+		}
+		++shift;
+	}
+	return (shift);
+}
+
 static void parse_shft_right_arrow(struct s_input *inp)
 {
-	
+	struct s_cursor	next_word;
+	size_t			shift;
+	size_t			pos;
+	char			*str;
+	// struct s_cursor	end;
+P_UNUSED(next_word);
+	// end = input_get_end_string(inp);
+	if (inp->gap.slide != inp->gap.len_string)
+	{
+		str = gap_get_buf(&inp->gap);
+		shift = get_skip_right_word(str, inp->gap.slide);
+		pos = inp->gap.slide + shift + inp->len_greet;
+		next_word.y = pos / inp->win.cols;
+		next_word.x = pos % inp->win.cols;
+		inp->cr.y = inp->save_refresh_pos.y + next_word.y;
+		inp->cr.x = next_word.x;
+		inp->gap.slide += shift;
+		ft_strdel(&str);
+	}
 }
 
 static size_t	get_skip_left_word(const char *str, size_t pos)
@@ -57,6 +96,7 @@ static void	parse_shft_left_arrow(struct s_input *inp)
 		inp->cr.y = inp->save_refresh_pos.y + prev_word.y;
 		inp->cr.x = prev_word.x;
 		inp->gap.slide -= shift;
+		ft_strdel(&str);
 	}
 }
 
