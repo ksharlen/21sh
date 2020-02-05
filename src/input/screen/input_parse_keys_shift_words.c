2a698b807c3ddf03a5c5e0b0dcf6bce4b3b534b1
ksharlen <miily@list.ru>
2020-02-06 00:07:28 +0300

    refact skip word functions

--- src/input/screen/input_parse_keys_shift_words.c
+++ src/input/screen/input_parse_keys_shift_words.c
@@ -6,7 +6,7 @@
 /*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/05 21:16:39 by ksharlen          #+#    #+#             */
-/*   Updated: 2020/02/05 23:50:31 by ksharlen         ###   ########.fr       */
+/*   Updated: 2020/02/06 00:05:55 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #define SKIP_L_WORD 0
 #define SKIP_R_WORD 1
 
-static size_t	get_shift_skip_word(const char *str, size_t pos, int whome)
+static size_t	get_shift_skip_word(const char *str, size_t pos, int where)
 {
 	size_t		shift;
 	const char	*p_str;
@@ -24,11 +24,11 @@ static size_t	get_shift_skip_word(const char *str, size_t pos, int whome)
 	if (str)
 	{
 		p_str = &str[pos];
-		while (whome == SKIP_L_WORD ? p_str != str : *p_str)
+		while (where == SKIP_L_WORD ? p_str != str : *p_str)
 		{
 			if (((*p_str) > 32 && (*p_str) < 126) && *(p_str - 1) == ' ')
 				break ;
-			whome == SKIP_L_WORD ? --p_str : ++p_str;
+			where == SKIP_L_WORD ? --p_str : ++p_str;
 			++shift;
 		}
 		++shift;
@@ -36,57 +36,46 @@ static size_t	get_shift_skip_word(const char *str, size_t pos, int whome)
 	return (shift);
 }
 
-static void parse_shft_right_arrow(struct s_input *inp)
+static void		get_coor_word(struct s_input *inp, ssize_t shift)
 {
-	struct s_cursor	next_word;
-	size_t			shift;
-	size_t			pos;
+	struct s_cursor	coord_word;
+	ssize_t			pos;
+
+	pos = inp->gap.slide + shift + inp->len_greet;
+	coord_word.y = pos / inp->win.cols;
+	coord_word.x = pos % inp->win.cols;
+	inp->cr.y = inp->save_refresh_pos.y + coord_word.y;
+	inp->cr.x = coord_word.x;
+	inp->gap.slide += shift;
+}
+
+static void		parse_shft_right_arrow(struct s_input *inp)
+{
 	char			*str;
+	ssize_t			shift;
 
-	if (inp->gap.slide != inp->gap.len_string)
-	{
-		str = gap_get_buf(&inp->gap);
-		shift = get_shift_skip_word(str, inp->gap.slide + 1, SKIP_R_WORD);
-		pos = inp->gap.slide + shift + inp->len_greet;
-		next_word.y = pos / inp->win.cols;
-		next_word.x = pos % inp->win.cols;
-		inp->cr.y = inp->save_refresh_pos.y + next_word.y;
-		inp->cr.x = next_word.x;
-		inp->gap.slide += shift;
-		ft_strdel(&str);
-	}
+	str = gap_get_buf(&inp->gap);
+	shift = get_shift_skip_word(str, inp->gap.slide + 1, SKIP_R_WORD);
+	get_coor_word(inp, shift);
+	ft_strdel(&str);
 }
 
-static void	parse_shft_left_arrow(struct s_input *inp)
+static void		parse_shft_left_arrow(struct s_input *inp)
 {
-	struct s_cursor	prev_word;
 	char	*str;
-	size_t	shift;
-	size_t	pos;
+	ssize_t	shift;
 
-	shift = 0;
-	if (inp->gap.slide)
-	{
-		str = gap_get_buf(&inp->gap);
-		shift = get_shift_skip_word(str, inp->gap.slide - 1, SKIP_L_WORD);
-		pos = inp->gap.slide - shift + inp->len_greet;
-		prev_word.y = pos / (inp->win.cols);
-		prev_word.x = pos % (inp->win.cols);
-		inp->cr.y = inp->save_refresh_pos.y + prev_word.y;
-		inp->cr.x = prev_word.x;
-		inp->gap.slide -= shift;
-		ft_strdel(&str);
-	}
+	str = gap_get_buf(&inp->gap);
+	shift = get_shift_skip_word(str, inp->gap.slide - 1, SKIP_L_WORD);
+	get_coor_word(inp, shift * (-1));
+	ft_strdel(&str);
 }
 
-void	input_parse_keys_shift_words(struct s_input *inp)
+void			input_parse_keys_shift_words(struct s_input *inp)
 {
-	if (inp->key == KEY_SHIFT_L_ARROW)
-	{
+	if (inp->key == KEY_SHIFT_L_ARROW && inp->gap.slide)
 		parse_shft_left_arrow(inp);
-	}
-	else if (inp->key == KEY_SHIFT_R_ARROW)
-	{
+	else if (inp->key == KEY_SHIFT_R_ARROW &&
+				inp->gap.slide != inp->gap.len_string)
 		parse_shft_right_arrow(inp);
-	}
 }
