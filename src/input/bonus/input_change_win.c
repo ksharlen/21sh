7e1ca4d44a2ab12f57b407876a38c7deeaad55f4
ksharlen <miily@list.ru>
2020-02-06 23:38:44 +0300

    added defense for save pos in 0 pos

--- src/input/bonus/input_change_win.c
+++ src/input/bonus/input_change_win.c
@@ -6,32 +6,37 @@
 /*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/06 23:24:34 by ksharlen          #+#    #+#             */
-/*   Updated: 2020/02/06 23:29:01 by ksharlen         ###   ########.fr       */
+/*   Updated: 2020/02/06 23:36:07 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "21sh_bonus.h"
 
-static void	input_update_stat_win(struct s_input *inp, struct s_win *wn)
+static ssize_t	get_reminder_row(struct s_input *inp, struct s_win *wn)
 {
 	ssize_t	curr_qt_rows;
 	ssize_t	chg_qt_rows;
-	ssize_t	reminder;
 
 	curr_qt_rows = ((inp->len_greet + inp->gap.len_string) / inp->win.cols) +
-	((inp->len_greet + inp->gap.len_string) % inp->win.cols ? 1 : 0);
+		((inp->len_greet + inp->gap.len_string) % inp->win.cols ? 1 : 0);
 	chg_qt_rows = (inp->len_greet + inp->gap.len_string) / wn->cols +
-	((inp->len_greet + inp->gap.len_string) % wn->cols ? 1 : 0);
-	reminder = curr_qt_rows - chg_qt_rows;
-	if (reminder)
+		((inp->len_greet + inp->gap.len_string) % wn->cols ? 1 : 0);
+	return (curr_qt_rows - chg_qt_rows);
+}
+
+static void	input_update_stat_win(struct s_input *inp, struct s_win *wn)
+{
+	ssize_t	reminder;
+
+	if (inp->save_refresh_pos.y)
 	{
-		if (reminder > 0)
+		reminder = get_reminder_row(inp, wn);
+		if (reminder)
 		{
-			inp->save_refresh_pos.y += FT_ABS(reminder);
-		}
-		else
-		{
-			inp->save_refresh_pos.y -= FT_ABS(reminder);
+			if (reminder > 0)
+				inp->save_refresh_pos.y += FT_ABS(reminder);
+			else
+				inp->save_refresh_pos.y -= FT_ABS(reminder);
 		}
 	}
 	inp->win = *wn;
@@ -47,10 +52,5 @@ void	check_change_winsize(struct s_input *inp)
 	input_tgetent();
 	wn = get_win_size();
 	if (ft_memcmp(&wn, &inp->win, sizeof(struct s_win)))
-	{
-		// inp->win = wn;
 		input_update_stat_win(inp, &wn);
-	}
-		// inp->win = wn;
 }
-
