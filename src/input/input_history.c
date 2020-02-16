75fd770433a5e5cbb3130942cacb14288edf1108
Kharazim Sharlene <ksharlen@mi-u1.21-school.ru>
2020-02-16 18:07:47 +0300

    refact input_history

--- src/input/input_history.c
+++ src/input/input_history.c
@@ -6,7 +6,7 @@
 /*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/08 17:21:22 by ksharlen          #+#    #+#             */
-/*   Updated: 2020/02/08 22:55:39 by ksharlen         ###   ########.fr       */
+/*   Updated: 2020/02/16 18:06:54 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,33 +15,21 @@
 void	input_history(struct s_input *inp)
 {
 	char	*str_hist;
+	char	*(*get_hist)(t_history *);
 
-	if (inp->key == KEY_UP_ARROW)
+	get_hist = (inp->key == KEY_UP_ARROW ?
+		history_get_top_cmd : history_get_bottom_cmd);
+	str_hist = get_hist(inp->hist);
+	if (str_hist)
 	{
-		str_hist = history_get_top_cmd(inp->hist);
-		if (str_hist)
-		{
-			inp->cr = inp->save_refresh_pos;
-			clear_pos_cr_to_the_end();
-			gap_clean_buf(&inp->gap);
-			gap_put_str(&inp->gap, str_hist);
-			inp->gap.slide = inp->gap.len_string;
-		}
+		inp->cr = inp->save_refresh_pos;
+		clear_pos_cr_to_the_end();
+		gap_clean_buf(&inp->gap);
+		gap_put_str(&inp->gap, str_hist);
+		inp->gap.slide = inp->gap.len_string;
 	}
 	else if (inp->key == KEY_DOWN_ARROW)
-	{
-		str_hist = history_get_bottom_cmd(inp->hist);
-		if (str_hist)
-		{
-			inp->cr = inp->save_refresh_pos;
-			clear_pos_cr_to_the_end();
-			gap_clean_buf(&inp->gap);
-			gap_put_str(&inp->gap, str_hist);
-			inp->gap.slide = inp->gap.len_string;
-		}
-		else
-			gap_clean_buf(&inp->gap);
-	}
+		gap_clean_buf(&inp->gap);
 	get_coor_word(inp, inp->greet.len + inp->gap.len_string);
 	refresh_screen(inp);
 }
