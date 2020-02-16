709ebda8edcf65117a38bbc8eed8467681136cb4
Kharazim Sharlene <ksharlen@mi-u1.21-school.ru>
2020-02-16 18:40:51 +0300

    nor internal_utilities

--- src/internal_utilities/iu_init_var_env.c
+++ src/internal_utilities/iu_init_var_env.c
@@ -1,31 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   21sh_init_env.c                                    :+:      :+:    :+:   */
+/*   iu_init_var_env.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/02/09 20:07:20 by ksharlen          #+#    #+#             */
-/*   Updated: 2020/02/09 20:39:34 by ksharlen         ###   ########.fr       */
+/*   Created: 2020/02/16 18:37:20 by ksharlen          #+#    #+#             */
+/*   Updated: 2020/02/16 18:40:23 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "internal_utilities.h"
 
-char    **g_sh_environ;
+char	**g_sh_environ;
 
-void    print_env(void)
+void	print_env(void)
 {
-    char    **p_environ;
-    char    *str;
+	char	**p_environ;
+	char	*str;
 
-    p_environ = g_sh_environ;
-    while (p_environ && *p_environ)
-    {
+	p_environ = g_sh_environ;
+	while (p_environ && *p_environ)
+	{
 		str = (*p_environ);
 		ft_printf("%s\n", str);
 		++p_environ;
-    }
+	}
 }
 
 void	clean_env(void)
@@ -33,13 +33,13 @@ void	clean_env(void)
 	ft_strdel_split(g_sh_environ);
 }
 
-void    init_env(char **env)
+void	init_env(char **env)
 {
-    if (env && *env)
-        g_sh_environ = ft_linedup(env);
-    else
-    {
-        g_sh_environ = (char **)ft_memalloc(sizeof(char *) * 1);
-        g_sh_environ[0] = NULL;
-    }
+	if (env && *env)
+		g_sh_environ = ft_linedup(env);
+	else
+	{
+		g_sh_environ = (char **)ft_memalloc(sizeof(char *) * 1);
+		g_sh_environ[0] = NULL;
+	}
 }
