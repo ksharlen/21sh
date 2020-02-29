d5164e9fe8a671d880dd62edda99533b8c731739
Kharazim Sharlene <ksharlen@at-g2.21-school.ru>
2020-02-29 21:11:15 +0300

    remaek exec

--- src/exec/cmd/run_cmd.c
+++ src/exec/cmd/run_cmd.c
@@ -7,14 +7,14 @@ static int	find_and_run_cmd(t_pars_list *list)
 	else if (!ft_strcmp("false", list->name_func))
 		list->status = cmd_false(list);
 	else if (!ft_strcmp("cd", list->name_func))
-		list->status = sh21_cd(0, NULL, NULL);	//////// замена
+		list->status = sh21_cd(0, list->pars_args, NULL);	//////// замена
 	else if (!ft_strcmp("echo", list->name_func))
-		list->status = sh21_echo(0, NULL, NULL);	//////// замена
+		list->status = sh21_echo(0, list->pars_args, NULL);	//////// замена
 	else if (!ft_strcmp("pwd", list->name_func))
-		list->status = sh21_pwd(0, NULL, NULL);	//////// замена
-	else if (!ft_strcmp("setenv", list->name_func))
+		list->status = sh21_pwd(0, list->pars_args, NULL);	//////// замена
+	else if (!ft_strcmp("setenv", list->name_func))//TODO: need think
 		list->status = sh21_setenv(NULL, NULL, 0);	//////// замена
-	else if (!ft_strcmp("unsetenv", list->name_func))
+	else if (!ft_strcmp("unsetenv", list->name_func))//TODO: this too
 		list->status = sh21_unsetenv(NULL);	//////// замена
 	// else if (!ft_strcmp("env", list->name_func))//TODO: need add
 		// list->status = minishell_env(0, NULL, NULL);	//////// замена
