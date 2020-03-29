d7d0daf9cc1df3cc750b274c9d6baeead7efd997
ksharlen <miily@list.ru>
2020-03-29 16:31:28 +0300

    fix seg fault

--- src/exec/cmd/run_cmd.c
+++ src/exec/cmd/run_cmd.c
@@ -6,33 +6,51 @@
 /*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/05 15:56:21 by mdelphia          #+#    #+#             */
-/*   Updated: 2020/03/29 16:01:30 by ksharlen         ###   ########.fr       */
+/*   Updated: 2020/03/29 16:30:08 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec.h"
 
+//TODO: need remake
+// static char	**skip_env_args(char **parse_list, int *argc)
+// {
+// 	int		i;
+// 	char	**new_args;
+// 	size_t	skip_args;
+// 	char	**cpy_prs;
+
+// 	i = 0;
+// 	skip_args = 0;
+// 	cpy_prs = parse_list;
+// 	while ((i < *argc) && parse_list[i] && (ft_strchr(parse_list[i], '=')))
+// 	{
+// 		++skip_args;
+// 		++parse_list;
+// 		++i;
+// 	}
+// 	new_args = ft_linedup(parse_list);
+// 	ft_strdel_split(cpy_prs);
+// 	*argc -= skip_args;
+// 	return (new_args);
+// }
+
 static char	**skip_env_args(char **parse_list, int *argc)
 {
-	int		i;
 	char	**new_args;
-	size_t	skip_args;
 	char	**cpy_prs;
 
-	i = 0;
-	skip_args = 0;
 	cpy_prs = parse_list;
-	while ((i < *argc) && parse_list[i] && (ft_strchr(parse_list[i], '=')))
+	while (parse_list && *parse_list)
 	{
-		++skip_args;
+		if (!ft_strchr(*parse_list, '='))
+			break ;
 		++parse_list;
-		++i;
+		--(*argc);
 	}
-	// printf("argc: %d\n", *argc);
 	// exit(EXIT_FAILURE);
 	new_args = ft_linedup(parse_list);
 	ft_strdel_split(cpy_prs);
-	*argc -= skip_args;
 	return (new_args);
 }
 
@@ -48,7 +66,7 @@ static char	**skip_env_flags(char **pars_list, int *argc)
 	cpy_prs = pars_list;
 	++pars_list;
 	--(*argc);
-	while ((int)i < *argc)
+	while ((int)i < *argc && *pars_list)
 	{
 		if (**pars_list == '-')
 		{
@@ -79,6 +97,8 @@ static void	exec_env(t_exec_lst execlist, t_pars_list *list)
 		check_run(execlist, &list);
 		g_sh_environ = ft_linedup(cpy_environ_src);
 	}
+	else
+		print_env();
 	ft_strdel_split(cpy_environ_src);
 }
 
