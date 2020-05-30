28acad5e0b77a217db0107089576bd61dc643733
OM-Ra <syndeft@gmail.com>
2020-05-30 17:47:24 +0300

    comand+D in main

--- src/main.c
+++ src/main.c
@@ -3,14 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
+/*   By: student <student@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/03 16:55:32 by ksharlen          #+#    #+#             */
-/*   Updated: 2020/05/20 22:48:43 by ksharlen         ###   ########.fr       */
+/*   Updated: 2020/05/20 22:48:43 by student          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sh.h"
+
+static void sh21_finish(char **sh_environ)
+{
+	ft_strdel_split(sh_environ);
+	free(sh_environ);
+}
 
 int			main(int argc, char **argv, char **env)
 {
@@ -23,11 +29,15 @@ int			main(int argc, char **argv, char **env)
 	while (1)
 	{
 		input_begin(&init.execlist, &init.inp);
-		if (init.inp.str_for_parse &&
+		if ((!init.inp.str_for_parse && init.inp.key == ('d' & 0x1f)))		// для сдачи проекта нужно удалить
+			break ;
+		else if (init.inp.str_for_parse &&
 			!parser(&init.execlist, &(init.inp.str_for_parse), &init.prs))
 				check_choice(&init.execlist, init.prs.beg);
 		free_befor_exec(&init.prs);
 	}
+	input_finish(&init.inp);
+	sh21_finish(init.execlist.sh_environ);
 	return (0);
 }
 
