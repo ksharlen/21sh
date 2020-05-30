c1a4dd52fb1772c38fbf43129e42307258e6b0ae
ksharlen <miily@list.ru>
2020-05-31 00:27:40 +0300

    fix history

--- src/history/history_move.c
+++ src/history/history_move.c
@@ -6,7 +6,7 @@
 /*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/02 21:51:34 by ksharlen          #+#    #+#             */
-/*   Updated: 2020/02/16 18:16:32 by ksharlen         ###   ########.fr       */
+/*   Updated: 2020/05/31 00:27:11 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,8 @@ char	*history_get_bottom_cmd(t_history *hist)
 
 char	*history_get_top_cmd(t_history *hist)
 {
-	if (hist->h_list->prev)
+	if (hist->h_list->prev && hist->h_list->prev->comand &&
+		(ft_strlen(hist->h_list->prev->comand) > 0))
 	{
 		hist->h_list = hist->h_list->prev;
 		return (hist->h_list->comand);
